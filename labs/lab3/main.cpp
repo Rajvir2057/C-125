b524551232b74f5b2090a882c55cd8d7fd203948
Rajvir2057 <rajvirkaur2057@gmail.com>
2025-10-17 22:23:14 -0700

    finished lab3

--- labs/lab3/main.cpp
+++ labs/lab3/main.cpp
@@ -1,21 +1,94 @@
 #include <iostream>
+#include <vector>
 #include "ArrayStack.h"
+#include "Task1.cpp"
+#include "task2.cpp"
+#include "LinkedStack.h"
 
-int main() {
-    ArrayStack<int> aStack;
-    aStack.push(10);
-    aStack.push(20);
-    aStack.push(30);
+int main(){
+    
+    //implementing task1.. this has O(n) output, why : we have two different while loops and each has valuve of n. 
+    //if it was nested then the output would be O(n^2)
+    
+    //creating the T1.1 where we push 10 20 30 and get 30 20 10.
+    ArrayStack <int> Stack1;
+    
+    Stack1.push(10);
+    Stack1.push(20);
+    Stack1.push(30);
+    
+    //creating a loop to see all and run.
+    std :: vector <int> T1 =  toVector(Stack1);
+    for(int i : T1){
+        std:: cout << i << ",";
+    }
+    
+    std:: cout << "\n" << "Returned to normal" << "\n"; 
+    
+    for(int i : T1){
+    std:: cout << Stack1.peek() << "," ;
+    Stack1.pop();
+    }
+    std:: cout << "\n";
+    
+    //making T1.2 return an empty stack.
+    ArrayStack <int> Stack2;
 
-    std::cout << "Top of aStack: " << aStack.peek() << '\n';
+    std :: vector <int> T2 = toVector(Stack2);
+    if(T2.empty()){
+        std:: cout << "empty" << "\n";
+    }
+    
+    //T1.3 giving 5 to the stack 
+    ArrayStack <int> Stack3;
 
-    aStack.pop();
-    std::cout << "After one pop, top: " << aStack.peek() << '\n';
+    Stack3.push(5);
 
-    ArrayStack<int> bStack;
-    std::cout << "Is bStack empty? " << std::boolalpha << bStack.isEmpty() << '\n';
+    std:: vector <int> T3 = toVector(Stack3);
 
-    ArrayStack<int> cStack;
-    cStack.push(5);
-    std::cout << "Top of cStack: " << cStack.peek() << '\n';
+    std :: cout << Stack3.peek() << "\n";
+
+    // all the above use O(n) since we have single uses of while and if statements to trigger output.
+
+    //task 2 implementation.
+
+    // Even this has O(n) , why : we are not using nested and just simply using a linkedchain to pop and peek with if logic from Task2.
+    //okie now we gonna get a min value after pushing.
+
+    MinStack smallvalue;
+
+    smallvalue.push(5);
+    smallvalue.push(2);
+    smallvalue.push(3);
+
+    std :: cout << "Small value is : " << smallvalue.getMin() << "\n";
+
+    //T2.2
+
+    MinStack value;
+
+    value.push(7);
+    value.pop();
+
+    if(value.isEmpty()){
+        std:: cout <<"Removed the digit 7." << "\n";
+    }
+
+    value.push (4);
+
+    std :: cout << value.getMin() <<"\n";
+
+    //t2.3
+    MinStack lasttest;
+
+    lasttest.push(8);
+    lasttest.push(6);
+
+    lasttest.pop();
+
+    lasttest.push(1);
+    
+    std :: cout << "get min = " << lasttest.getMin() << "\n";
+
+    // even this is o(n) , simple pop and push is used from single if statements.
 }
--- labs/lab3/task2.cpp
+++ labs/lab3/task2.cpp
@@ -1,29 +1,49 @@
    #include <iostream>
    #include "LinkedStack.h"
    #include "StackInterface.h"
-   
-   
-   
+   #include "Node.h"
+
    class MinStack {
+
         private:
-            Stack<int> dataStack; // normal stack
-            Stack<int> minStack; // tracks current minimums
 
+            LinkedStack<int> dataStack; // normal stack
+            LinkedStack<int> minStack; // tracks current minimums
         public:
+
             void push(const int& x) {
-                // TODO
-            }
-            void pop() {
-                // TODO
-            }
-            void top() const {
-                // TODO
-            }
-            void getMin() const {
-                // TODO
+
+                dataStack.push(x); // here we will create all containing the x and min will take and keep the small one.
+                //taking care of taking only less min.4
+
+                if(minStack.isEmpty() || x <= minStack.peek() ){
+                    minStack.push(x);
+                }
+
             }
 
-            bool empty() const {
+            void pop() {
+                //peek to the top first.
+                int y = dataStack.peek();
+                dataStack.pop();
+               if(y  == minStack.peek()) {
+                    minStack.pop();
+            }
+            // pop is to remove and check for less here.here LIFO is applied 
+            }
+
+            int top() const {
+                // here we will use peek just to look at the top element.
+
+               return dataStack.peek();
+
+            }
+
+            int getMin() const {
+                return minStack.peek();
+            }
+
+            bool isEmpty() const {
                 return dataStack.isEmpty();
             }
     };
